00dd9c00ee2fc5fe1ed5e7b2bfbf4ed6e54a146e
Jim Huang <jserv.tw@gmail.com>
2024-02-07 20:38:45 +0800

    Modernize disaout source files

--- tools/disaout/main.c
+++ tools/disaout/main.c
@@ -4,75 +4,74 @@ extern int load_a_out(const char *file, struct exec *E);
 extern int printins(int addr);
 extern void patch_symbols(void);
 extern void print_symtables(void);
-extern u_int8_t *ispace, *dspace;	/* Instruction and Data spaces */
+extern u_int8_t *ispace, *dspace; /* Instruction and Data spaces */
 extern int doprint;
-int onepass = 0;		/* Only do a single pass */
-int printaddrs = 0;		/* Print out addresses and words */
+int onepass = 0;    /* Only do a single pass */
+int printaddrs = 0; /* Print out addresses and words */
 
 void dopass(struct exec *e)
 {
-  int i;
-  u_int16_t *iptr;
-  for (i = e->a_entry; i < e->a_entry + e->a_text;) {
-    iptr = (u_int16_t *) & ispace[i];
-    if (doprint && printaddrs)
-      printf("%06o: %06o\t", i, *iptr);
-    i += printins(i);
-  }
+    u_int16_t *iptr;
+    for (int i = e->a_entry; i < e->a_entry + e->a_text;) {
+        iptr = (u_int16_t *) &ispace[i];
+        if (doprint && printaddrs)
+            printf("%06o: %06o\t", i, *iptr);
+        i += printins(i);
+    }
 }
 
 void usage()
 {
-  fprintf(stderr, "Usage: disaout [-1a] file\n");
-  exit(1);
+    fprintf(stderr, "Usage: disaout [-1a] file\n");
+    exit(1);
 }
 
 int main(int argc, char *argv[])
 {
-  struct exec E;
-  int ch, err;
+    struct exec E;
+    int ch, err;
 
-  /* Get any arguments */
-  while ((ch = getopt(argc, argv, "1a")) != -1) {
-    switch (ch) {
-    case '1':
-      onepass = 1;
-      break;
-    case 'a':
-      printaddrs = 1;
-      break;
-    case '?':
-    default:
-      usage();
+    /* Get any arguments */
+    while ((ch = getopt(argc, argv, "1a")) != -1) {
+        switch (ch) {
+        case '1':
+            onepass = 1;
+            break;
+        case 'a':
+            printaddrs = 1;
+            break;
+        case '?':
+        default:
+            usage();
+        }
     }
-  }
-  argc -= optind;
-  argv += optind;
+    argc -= optind;
+    argv += optind;
 
+    /* Check we have an file to open */
+    if (argc != 1)
+        usage();
 
-  /* Check we have an file to open */
-  if (argc != 1)
-    usage();
+    /* Get the header details for the a.out file */
+    err = load_a_out(argv[0], &E);
 
-  /* Get the header details for the a.out file */
-  err = load_a_out(argv[0], &E);
+    if (err == -1) {
+        fprintf(stderr, "%s does not appear to be a PDP-11 a.out file\n",
+                argv[0]);
+        exit(1);
+    }
 
-  if (err == -1) {
-    fprintf(stderr, "%s does not appear to be a PDP-11 a.out file\n", argv[0]);
-    exit(1);
-  }
+    printf("/ text at 0%o, len 0%o, end 0%o\n", E.a_entry, E.a_text,
+           E.a_entry + E.a_text);
 
-  printf("/ text at 0%o, len 0%o, end 0%o\n",
-	E.a_entry, E.a_text, E.a_entry + E.a_text);
+    if (onepass == 0) {
+        doprint = 0;
+        dopass(&E); /* Do pass 1 to infer symbols */
+        patch_symbols();
+        /* print_symtables(); */
+    }
+    doprint = 1;
+    dopass(&E); /* Do pass 2 to print it out */
 
-  if (onepass == 0) {
-    doprint = 0;
-    dopass(&E);			/* Do pass 1 to infer symbols */
-    patch_symbols();
-    /* print_symtables(); */
-  }
-  doprint = 1;
-  dopass(&E);			/* Do pass 2 to print it out */
-
-  exit(0);
+    exit(0);
 }
--- tools/disaout/symbols.c
+++ tools/disaout/symbols.c
@@ -2,52 +2,51 @@
 
 #include "aout.h"
 
-struct symbol * isym[PDP_MEM_SIZE],  * dsym[PDP_MEM_SIZE];
+struct symbol *isym[PDP_MEM_SIZE], *dsym[PDP_MEM_SIZE];
 
 /* Array of structs holding details for each type of symbol */
 struct symtype {
-  char *format;
-  int counter;
-  struct symbol **table;
+    char *format;
+    int counter;
+    struct symbol **table;
 } symtypelist[] = {
-	{ "%d", 1, isym },
-	{ "func%d", 1, isym },
-	{ "data%d", 1, dsym },
-	{ "jsrtext%d", 1, isym },
-	{ "jsrdata%d", 1, isym },
+    {"%d", 1, isym},        {"func%d", 1, isym},    {"data%d", 1, dsym},
+    {"jsrtext%d", 1, isym}, {"jsrdata%d", 1, isym},
 };
 
 /* Debug code */
 void print_symtables(void)
 {
-  int i;
-  for (i=0; i< PDP_MEM_SIZE; i++) {
-    if (isym[i]!=NULL)
-      printf("0%06o %d %d %s\n",i, isym[i]->type, isym[i]->size, isym[i]->name);
-    if (dsym[i]!=NULL)
-      printf("0%06o %d %d %s\n",i, dsym[i]->type, dsym[i]->size, dsym[i]->name);
-  }
+    for (int i = 0; i < PDP_MEM_SIZE; i++) {
+        if (isym[i] != NULL)
+            printf("0%06o %d %d %s\n", i, isym[i]->type, isym[i]->size,
+                   isym[i]->name);
+        if (dsym[i] != NULL)
+            printf("0%06o %d %d %s\n", i, dsym[i]->type, dsym[i]->size,
+                   dsym[i]->name);
+    }
 }
 
 void add_symbol(int addr, int type, int size)
 {
-  struct symbol *s;
+    struct symbol *s;
 
-  /* See if we have a symbol already defined at this address */
-  /* If there is one and its type is >= ours, don't put a new one in */
-  if ((symtypelist[type].table[addr] != NULL) &&
-      (symtypelist[type].table[addr]->type >= type)) return;
+    /* See if we have a symbol already defined at this address */
+    /* If there is one and its type is >= ours, don't put a new one in */
+    if ((symtypelist[type].table[addr] != NULL) &&
+        (symtypelist[type].table[addr]->type >= type))
+        return;
 
-  /* No, so create one */
-  s= malloc(sizeof(struct symbol));
+    /* No, so create one */
+    s = malloc(sizeof(struct symbol));
 #if 0
   s->name= malloc(12);
   snprintf(s->name,12,symtypelist[type].format, symtypelist[type].counter++);
 #endif
-  s->name= NULL;			/* To be filled in later */
-  s->type= type;
-  s->size= size;
-  symtypelist[type].table[addr]= s;
+    s->name = NULL; /* To be filled in later */
+    s->type = type;
+    s->size = size;
+    symtypelist[type].table[addr] = s;
 }
 
 /* Walk through both isym and dsym tables, giving them actual
@@ -55,81 +54,88 @@ void add_symbol(int addr, int type, int size)
  */
 void patch_symbols(void)
 {
-  int i,type;
-  struct symbol *s;
-  for (i=0; i< PDP_MEM_SIZE; i++) {
-    if (isym[i]==NULL) continue;
-    s= isym[i];
-    if (s->name != NULL) continue;
-    type=s->type;
-    s->name= malloc(12);
-    snprintf(s->name,12,symtypelist[type].format, symtypelist[type].counter++);
-  }
-  for (i=0; i< PDP_MEM_SIZE; i++) {
-    if (dsym[i]==NULL) continue;
-    s= dsym[i]; s->name= malloc(12);
-    type=s->type;
-    snprintf(s->name,12,symtypelist[type].format, symtypelist[type].counter++);
-  }
+    int i, type;
+    struct symbol *s;
+    for (i = 0; i < PDP_MEM_SIZE; i++) {
+        if (isym[i] == NULL)
+            continue;
+        s = isym[i];
+        if (s->name != NULL)
+            continue;
+        type = s->type;
+        s->name = malloc(12);
+        snprintf(s->name, 12, symtypelist[type].format,
+                 symtypelist[type].counter++);
+    }
+    for (i = 0; i < PDP_MEM_SIZE; i++) {
+        if (dsym[i] == NULL)
+            continue;
+        s = dsym[i];
+        s->name = malloc(12);
+        type = s->type;
+        snprintf(s->name, 12, symtypelist[type].format,
+                 symtypelist[type].counter++);
+    }
 }
 
-struct symbol * get_isym(int addr)
+struct symbol *get_isym(int addr)
 {
-  return(isym[addr]);
+    return (isym[addr]);
 }
 
-struct symbol * get_dsym(int addr)
+struct symbol *get_dsym(int addr)
 {
-  return(dsym[addr]);
+    return (dsym[addr]);
 }
 
 /* Walk the 0407 symbol table and load the symbols found */
 void load_0407_symbols(FILE *zin, int offset, int size, int base)
 {
-  struct sym0407 S;
-  struct symbol *oursym;
-  char name[9];
-  long curpos;
+    struct sym0407 S;
+    struct symbol *oursym;
+    char name[9];
+    long curpos;
 
-  /* Record where we are, and seek to the symbol table */
-  curpos= ftell(zin);
-  if (fseek(zin, offset, SEEK_SET) != 0) {
-    printf("Unable to load symbol table at offset %d\n", offset);
-    return;
-  }
-  name[8]='\0';		/* Ensure we get a properly terminated string */
-
-  /* Walk the list */
-  while (size>0) {
-    fread(&S, sizeof(S), 1, zin); size -= sizeof(S);
-    memcpy(name, S.name, 8);
-    S.addr += base;
-
-    switch (S.type) {
-      case ASYM_DATA:
-      case ASYM_BSS:
-      case ASYM_DATAEXT:
-      case ASYM_BSSDEXT:
-			oursym= malloc(sizeof(struct symbol));
-  			oursym->name= strdup(name);
-  			oursym->type= SYM_DATA;
-  			oursym->size= 0;
-  			dsym[S.addr]= oursym;
-			break;
-
-      case ASYM_TEXT:
-      case ASYM_TEXTEXT:
-			oursym= malloc(sizeof(struct symbol));
-  			oursym->name= strdup(name);
-			/* If it starts with l[0-9], it's a branch */
-			if (name[0]=='l' && name[1] >= '0' &&  name[1] <= '9')
-  			  oursym->type= SYM_BRANCH;
-			else
-  			  oursym->type= SYM_FUNCTION;
-  			oursym->size= 0;
-  			isym[S.addr]= oursym;
-			break;
+    /* Record where we are, and seek to the symbol table */
+    curpos = ftell(zin);
+    if (fseek(zin, offset, SEEK_SET) != 0) {
+        printf("Unable to load symbol table at offset %d\n", offset);
+        return;
     }
-  }
-  fseek(zin, curpos, SEEK_SET);
+    name[8] = '\0'; /* Ensure we get a properly terminated string */
+
+    /* Walk the list */
+    while (size > 0) {
+        fread(&S, sizeof(S), 1, zin);
+        size -= sizeof(S);
+        memcpy(name, S.name, 8);
+        S.addr += base;
+
+        switch (S.type) {
+        case ASYM_DATA:
+        case ASYM_BSS:
+        case ASYM_DATAEXT:
+        case ASYM_BSSDEXT:
+            oursym = malloc(sizeof(struct symbol));
+            oursym->name = strdup(name);
+            oursym->type = SYM_DATA;
+            oursym->size = 0;
+            dsym[S.addr] = oursym;
+            break;
+
+        case ASYM_TEXT:
+        case ASYM_TEXTEXT:
+            oursym = malloc(sizeof(struct symbol));
+            oursym->name = strdup(name);
+            /* If it starts with l[0-9], it's a branch */
+            if (name[0] == 'l' && name[1] >= '0' && name[1] <= '9')
+                oursym->type = SYM_BRANCH;
+            else
+                oursym->type = SYM_FUNCTION;
+            oursym->size = 0;
+            isym[S.addr] = oursym;
+            break;
+        }
+    }
+    fseek(zin, curpos, SEEK_SET);
 }
